3a3b2ef7150b69644675060c2ef31f64842159fe
Richard Garcia <richardgarciaph@gmail.com>
2017-11-13 13:27:19 +1300

    add addTask action

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -1,4 +1,11 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    addTask: function(){
+      var title = this.get('title');
+      var description = this.get('description');
+      var date = this.get('date');
+    }
+  }
 });
