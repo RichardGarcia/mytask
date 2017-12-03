276f7a76c2caaeafd9a716d534bb5d38360e7fca
Richard Garcia <richardgarciaph@gmail.com>
2017-12-03 23:05:05 +1300

    add edit function and update depricated ember stuff

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -5,3 +5,13 @@ export function formatDate(params) {
 }
 
 export default helper(formatDate);
+
+
+
+// import Ember from 'ember';
+
+// export function formatDate(params){
+// 	return moment(params[0]).format('YYYY-MM-DD');
+// }
+
+// export default Ember.Helper.helper(formatDate);
--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('tasks', function() {
     this.route('new');
+    this.route('edit', {path: 'edit/:task_id'});
+    // this.route('edit', {path: '/:task_id/edit'});
   });
 });
 
